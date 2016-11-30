437b6666df433ea93e1e382bc94217779a6cd302
Roman Bugaian <romanbugaian@bau36-3-ipv4-0056.misc.uni-kl.de>
2016-11-30 18:26:16 +0100

    Project commit

---
+++ app/src/androidTest/java/xyz/rbugaian/jenkinstestapp/ExampleInstrumentedTest.java
@@ -0,0 +1,32 @@
+package xyz.rbugaian.jenkinstestapp;
+
+import android.content.Context;
+import android.support.test.InstrumentationRegistry;
+import android.support.test.runner.AndroidJUnit4;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import static org.junit.Assert.*;
+
+/**
+ * Instrumentation test, which will execute on an Android device.
+ *
+ * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
+ */
+@RunWith(AndroidJUnit4.class)
+public class ExampleInstrumentedTest {
+    @Test
+    public void useAppContext() throws Exception {
+        // Context of the app under test.
+        Context appContext = InstrumentationRegistry.getTargetContext();
+
+        assertEquals("xyz.rbugaian.jenkinstestapp", appContext.getPackageName());
+    }
+
+    @Test
+    public void testAddition() throws Exception {
+        int two = 2;
+        assertEquals(two + two, 4);
+    }
+}
